2c55883d422b996c42c5b5a2959fb87481dace7c
Thibaut Assus <thibaut.assus@gmail.com>
2011-12-05 22:23:58 +0100

    steps

---
+++ features/step_definitions/post_steps.rb
@@ -0,0 +1,15 @@
+Given /^I am on the homepage$/ do
+  visit(root_path)
+end
+
+When /^I fill in "([^"]*)" with "([^"]*)"$/ do |label, content|
+  fill_in label, :with => content
+end
+
+When /^I press "([^"]*)"$/ do |label|
+  click_button(label)
+end
+
+Then /^I should be thanked successfully$/ do
+  page.must_have_content('merci beaucoup pour proposition !')
+end
