d99b9d6ab3714005bf22b79953d774b28fae7b47
Thibaut Assus <thibaut.assus@gmail.com>
2011-12-05 22:17:34 +0100

    fist commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppelAPresentations::Application.config.secret_token = 'c20733eb319370e62e160d60277826ab048093d5f4192eeef1efb1a1b4e3b05f4e9356e2c7c5ff650148694870f78d65fa55fb12c9ab5c37c06d98ca64726ed0'
