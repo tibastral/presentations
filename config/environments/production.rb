f95bd35dd002a8db0e384b8f02e1d8c7a36334e3
Thibaut Assus <thibaut.assus@gmail.com>
2012-01-14 20:05:05 +0100

    with js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ AppelAPresentations::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += ['active_admin.css']
+  config.assets.precompile += ['active_admin.css', 'active_admin.js']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
