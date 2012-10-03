57eff510b921edcd7ce6d94bcf2ce09d7bd32834
Thibaut Assus <thibaut.assus@gmail.com>
2012-10-03 18:12:25 +0200

    Show more relevant talk listing (opacity in old talks)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,14 @@ module ApplicationHelper
       image_tag "http://bit.ly/github-red-banner-right", :alt => "Fork me on GitHub"
     end
   end
+
+  def format_oldness created_at
+    if created_at < 2.months.ago
+      "more-than-two-month-old"
+    elsif created_at < 1.month.ago
+      "more-than-one-month-old"
+    else
+      ""
+    end
+  end
 end
