1a7fea46d4939dd62583098231efd85114246f72
Thibaut Assus <thibaut.assus@gmail.com>
2012-10-03 18:59:03 +0200

    Better activeadmin presentation

--- app/admin/presentations.rb
+++ app/admin/presentations.rb
@@ -1,3 +1,4 @@
 ActiveAdmin.register Presentation do
-  
+  scope :all, default: true
+  scope :did_not_occur_yet
 end
--- app/models/presentation.rb
+++ app/models/presentation.rb
@@ -2,4 +2,8 @@ class Presentation < ActiveRecord::Base
   validates_presence_of :name
   validates_presence_of :title
   validates_presence_of :email
+
+  def self.did_not_occur_yet
+    where {occured_at == nil}
+  end
 end
