a450a85a23ab8f57a50711e8f41956f52c9334e0
Thibaut Assus <thibaut.assus@gmail.com>
2013-03-08 16:09:47 +0100

    Session fixed :)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
-    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"].to_s) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
     redirect_to root_url, :notice => "Authentification réussie !"
   end
